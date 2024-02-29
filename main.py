f2cbc5c4497a468a7545bf62d1f98819d658596a
Suraj Sharma <er.surajsharma1389@gmail.com>
2024-02-29 12:58:06 +0545

    Trying to Deploy the backend

--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from userdir.routes import UserRoutes
-from db import ping_server
+import uvicorn
 
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-async def on_start():
-    await ping_server()
-
-
-allowed_origins = [
-    "http://31.44.2.38",
-    "https://31.44.2.38",
-    "http://31.44.2.38:8002",
-    "http://localhost:5173"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins = allowed_origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
-)
-usr_router = UserRoutes()
-usr_router.setup_routes()
-
-app.include_router(usr_router.router)
+if __name__ == "__main__":
+    uvicorn.run("app:app",  host = "0.0.0.0", port = 8000, reload = True)
